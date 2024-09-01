ce7556a5a99e293fea5b955378d689d835d0c333
Sunillgg <t4093973@gmail.com>
2024-09-01 14:49:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7406707561:AAFtvnFTSYqKOGVCJpYLPV4N4X3QVcMiqBY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sunillgg")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6898426626")
 
 
 
